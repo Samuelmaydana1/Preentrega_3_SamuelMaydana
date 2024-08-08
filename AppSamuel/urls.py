acbbd00f4247e14e72686f6ef25924406e65a062
Samuel M <samuelmaydana1@gmail.com>
2024-08-08 13:50:17 -0300

    AppSamuel/urls ordenamiento

--- AppSamuel/urls.py
+++ AppSamuel/urls.py
@@ -4,19 +4,31 @@ from AppSamuel import views
 urlpatterns = [
     path('', views.inicio, name='Inicio' ),
     path('about', views.about, name='About'),
+]
+
+#Autor
+urlpatterns += [
     path('autor/listar', views.AutorListView.as_view(), name='ListaAutores'),
     path('autor/nuevo',views.AutorCreateView.as_view(), name='NuevoAutor'),
-    path('autor/<pk>',views.AutorDetailView.as_view(), name='DetalleAutor'),
-    path('autor/<pk>/editar',views.AutorUpdateView.as_view(), name='EditarAutor'),
-    path('autor/<pk>/borrar',views.AutorDeleteView.as_view(), name='BorrarAutor'),
+    path('autor/<int:pk>',views.AutorDetailView.as_view(), name='DetalleAutor'),
+    path('autor/<int:pk>/editar',views.AutorUpdateView.as_view(), name='EditarAutor'),
+    path('autor/<int:pk>/borrar',views.AutorDeleteView.as_view(), name='BorrarAutor')
+]
+
+#Categoría
+urlpatterns += [
     path('categoria/listar', views.CategoriaListView.as_view(), name='ListaCategorias'),
     path('categoria/nuevo',views.CategoriaCreateView.as_view(), name='NuevaCategoria'),
-    path('categoria/<pk>',views.CategoriaDetailView.as_view(), name='DetalleCategoria'),
-    path('categoria/<pk>/editar',views.CategoriaUpdateView.as_view(), name='EditarCategoria'),
-    path('categoria/<pk>/borrar',views.CategoriaDeleteView.as_view(), name='BorrarCategoria'),
+    path('categoria/<int:pk>',views.CategoriaDetailView.as_view(), name='DetalleCategoria'),
+    path('categoria/<int:pk>/editar',views.CategoriaUpdateView.as_view(), name='EditarCategoria'),
+    path('categoria/<int:pk>/borrar',views.CategoriaDeleteView.as_view(), name='BorrarCategoria')
+]
+
+#Libro
+urlpatterns += [
     path('libro/listar', views.LibroListView.as_view(), name='ListaLibros'),
     path('libro/nuevo',views.LibroCreateView.as_view(), name='NuevoLibro'),
-    path('libro/<pk>',views.LibroDetailView.as_view(), name='DetalleLibro'),
-    path('libro/<pk>/editar',views.LibroUpdateView.as_view(), name='EditarLibro'),
-    path('libro/<pk>/borrar',views.LibroDeleteView.as_view(), name='BorrarLibro'),
+    path('libro/<int:pk>',views.LibroDetailView.as_view(), name='DetalleLibro'),
+    path('libro/<int:pk>/editar',views.LibroUpdateView.as_view(), name='EditarLibro'),
+    path('libro/<int:pk>/borrar',views.LibroDeleteView.as_view(), name='BorrarLibro')
 ]
